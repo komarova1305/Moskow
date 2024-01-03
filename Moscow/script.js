7ca48345bf7c5c867b75d3d3d76867e6bd6c2897
Комарова Мария <149534401+komarova1305@users.noreply.github.com>
2024-01-03 18:32:05 +0300

    Update script.js

--- Moscow/script.js
+++ Moscow/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
   $('.header_burger').click(function(event) {
-    $('.header_burger, .burger_nav_links').toggleClass('active');
+    $('.header_burger, .burger_nav_links, .line1, .line2, .line3').toggleClass('active');
   });
 });
